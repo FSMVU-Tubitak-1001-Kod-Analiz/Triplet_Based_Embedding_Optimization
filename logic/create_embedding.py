edbb60923434f48118bb52d0334ff24a41f4dc0c
code1001_ws <@>
2024-02-02 00:00:01 +0300

    auto commit at Cum 02 Şub 2024 00:00:01 +03

--- logic/create_embedding.py
+++ logic/create_embedding.py
@@ -5,9 +5,9 @@ def create_embedding_codebert(code_path, file_name, batch_size=64):
     embeds.build_codebert(code_path, file_name, batch_size)
 
 
-def create_embedding_graphcodebert(code_path, file_name, batch_size=None):
+def create_embedding_graphcodebert(code_path, file_name, batch_size=64):
     embeds.build_graphcodebert(code_path, file_name, batch_size)
 
 
-def create_embedding_bert_nli_mean(code_path, file_name, batch_size=None):
+def create_embedding_bert_nli_mean(code_path, file_name, batch_size=64):
     embeds.build_bert_nli_mean(code_path, file_name, batch_size)
