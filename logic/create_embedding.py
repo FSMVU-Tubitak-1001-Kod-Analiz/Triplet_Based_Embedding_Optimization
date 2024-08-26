733715757a1518d71755eeebe8e4c91c814a5949
code1001_ws <@>
2024-08-27 00:00:01 +0300

    auto commit at Sal 27 Ağu 2024 00:00:01 +03

--- logic/create_embedding.py
+++ logic/create_embedding.py
@@ -11,3 +11,6 @@ def create_embedding_graphcodebert(code_path, file_name, batch_size=64):
 
 def create_embedding_bert_nli_mean(code_path, file_name, batch_size=64):
     embeds.build_bert_nli_mean(code_path, file_name, batch_size)
+
+if __name__ == "__main__":
+    create_embedding_codebert("../data/raw/unique_data_setV3.json", "temp", 64)
