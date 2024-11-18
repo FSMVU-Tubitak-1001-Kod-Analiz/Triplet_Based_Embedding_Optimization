2925acb199d1a287f79f1375890dd1690e812624
code1001_ws <@>
2024-11-19 00:00:01 +0300

    auto commit at Sal 19 Kas 2024 00:00:01 +03

--- parallel_call_V2.py
+++ parallel_call_V2.py
@@ -1,5 +1,6 @@
 import itertools
 import os
+from asyncio import as_completed
 from concurrent.futures import ProcessPoolExecutor, wait
 
 import numpy as np
@@ -12,7 +13,10 @@ def call_run(call_str):
     os.system(call_str)
 
 
-def call_parallel(embeds_paths, label_path, parameters=None):
+def call_parallel(embeds_paths, label_path, parameters=None, max_workers=None, non_perm_parameters=None):
+    if non_perm_parameters is None:
+        non_perm_parameters = {}
+
     if parameters is None:
         parameters = {
             "optimizer": ["SGD", "Adam"],
@@ -20,13 +24,18 @@ def call_parallel(embeds_paths, label_path, parameters=None):
             "lr": np.arange(-6, -3, 1)
         }
     else:
-        assert all(i in param_keys for i in parameters.keys())
+        assert all(i in parameters.keys() for i in param_keys)
 
-    output_folder = [i for i in os.listdir("results/") if i.startswith("hyperparam_run")]
-    output_folder = "results/hyperparam_run" + str(len(output_folder) + 1)
+    if "output_folder" not in parameters.keys():
+        output_folder = [i for i in os.listdir("results/") if i.startswith("hyperparam_run")]
+        output_folder = "results/hyperparam_run" + str(len(output_folder) + 1)
+        # output_folder = parameters.pop("output_folder", output_folder)
+    else:
+        output_folder = parameters["output_folder"]
     print(">> At folder", output_folder)
 
-    with ProcessPoolExecutor() as executor:
+    with ProcessPoolExecutor(max_workers=max_workers) as executor:
+        futures = []
         for embeds_path in embeds_paths:
             s = [np.arange(len(i)) for i in parameters.values()]
             perm = list(itertools.product(*s))
@@ -37,8 +46,9 @@ def call_parallel(embeds_paths, label_path, parameters=None):
                     "smell_range": ",".join(str(i) for i in (0, 7)),
                     "shuffle": 1,
                     "num_epochs": 2000,
-                    "patience": 500
+                    "patience": 2000
                 }
+                current_params = current_params | non_perm_parameters
                 for index, key in enumerate(parameters.keys()):
                     current_params[key] = parameters[key][p[index]]
 
@@ -47,7 +57,9 @@ def call_parallel(embeds_paths, label_path, parameters=None):
                             f"-sr {current_params['smell_range']} -sh {current_params['shuffle']} "
                             f"-e {current_params['num_epochs']} -pt {current_params['patience']} "
                             f"-o {output_folder}")
-                executor.submit(call_run, call_str)
+                futures.append(executor.submit(call_run, call_str))
+
+        wait(futures)
 
 
 def call_siamese_triplets():
@@ -58,7 +70,7 @@ def call_siamese_triplets():
 /home/eislamoglu/PycharmProjects/siamese-triplet/results/13/triplet_2024_11_16__21_23_51_7500_smells_codebert_pooler_output
 /home/eislamoglu/PycharmProjects/siamese-triplet/results/13/triplet_2024_11_16__21_29_31_7500_smells_codebert_pooler_output
 /home/eislamoglu/PycharmProjects/siamese-triplet/results/13/triplet_2024_11_16__21_50_04_7500_smells_graphcodebert_pooler_output
-/home/eislamoglu/PycharmProjects/siamese-triplet/results/13/triplet_2024_11_16__21_53_48_7500_smells_codebert_pooler_output
+|/home/eislamoglu/PycharmProjects/siamese-triplet/results/13/triplet_2024_11_16__21_53_48_7500_smells_codebert_pooler_output
 /home/eislamoglu/PycharmProjects/siamese-triplet/results/13/triplet_2024_11_16__21_53_59_7500_smells_bert_nli_mean_token_pooler_output""".split("\n")
 
     embed_type = "test"
@@ -72,11 +84,71 @@ def call_siamese_triplets():
     parameters = {
         "optimizer": ["Adam"],
         "train_batch_size": [256],
-        "lr": [-4]
+        "lr": [-4],
     }
 
     call_parallel(embeds_paths, label_path, parameters=parameters)
 
 
+def call_offline():
+    java_embeds = """/home/user/Desktop/Triplet-net-keras/Test/revision/java_bert_nli_mean_token_pooler_output.npy
+/home/user/Desktop/Triplet-net-keras/Test/revision/java_codebert_pooler_output.npy
+/home/user/Desktop/Triplet-net-keras/Test/revision/java_graphcodebert_pooler_output.npy
+/home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_bert_nli_mean_token_pooler_output_java.npy
+/home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_codebert_pooler_output_java.npy
+/home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_graphcodebert_pooler_output_java.npy
+/home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_graphcodebert_hidden_state_java.npy""".split("\n")
+
+    php_embeds = """/home/user/Desktop/Triplet-net-keras/Test/revision/php_bert_nli_mean_token_pooler_output.npy
+/home/user/Desktop/Triplet-net-keras/Test/revision/php_codebert_pooler_output.npy
+/home/user/Desktop/Triplet-net-keras/Test/revision/php_graphcodebert_pooler_output.npy
+/home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_bert_nli_mean_token_pooler_output_php.npy
+/home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_codebert_pooler_output_php.npy
+/home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_graphcodebert_pooler_output_php.npy
+/home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_graphcodebert_hidden_state_php.npy""".split("\n")
+
+    py_embeds = """/home/user/Desktop/Triplet-net-keras/Test/revision/py_bert_nli_mean_token_pooler_output.npy
+/home/user/Desktop/Triplet-net-keras/Test/revision/py_codebert_pooler_output.npy
+/home/user/Desktop/Triplet-net-keras/Test/revision/py_graphcodebert_pooler_output.npy
+/home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_bert_nli_mean_token_pooler_output_py.npy
+/home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_codebert_pooler_output_py.npy
+/home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_graphcodebert_pooler_output_py.npy
+/home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_graphcodebert_hidden_state_py.npy""".split("\n")
+
+#     java_embeds = """/home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_bert_nli_mean_token_pooler_output_java.npy
+# /home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_codebert_pooler_output_java.npy
+# /home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_graphcodebert_pooler_output_java.npy
+# /home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_graphcodebert_hidden_state_java.npy""".split("\n")
+#
+#     php_embeds = """/home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_bert_nli_mean_token_pooler_output_php.npy
+# /home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_codebert_pooler_output_php.npy
+# /home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_graphcodebert_pooler_output_php.npy
+# /home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_graphcodebert_hidden_state_php.npy""".split("\n")
+#
+#     py_embeds = """/home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_bert_nli_mean_token_pooler_output_py.npy
+# /home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_codebert_pooler_output_py.npy
+# /home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_graphcodebert_pooler_output_py.npy
+# /home/user/PycharmProjects/Model_Scratch/data/revision/500_smells_graphcodebert_hidden_state_py.npy""".split("\n")
+
+    parameters = {
+        "optimizer": ["Adam"],
+        "train_batch_size": [256],
+        "lr": [-4],
+    }
+
+    non_perm_parameters = {
+        "output_folder": "results/revision/v2"
+    }
+
+    java_labels_path = "data/raw/7500_smells_test_java.json"
+    call_parallel(java_embeds[:-1], java_labels_path, parameters=parameters, non_perm_parameters=non_perm_parameters)
+
+    php_labels_path = "data/raw/7500_smells_test_php.json"
+    call_parallel(php_embeds[:-1], php_labels_path, parameters=parameters, non_perm_parameters=non_perm_parameters)
+
+    py_labels_path = "data/raw/7500_smells_test_py.json"
+    call_parallel(py_embeds[:-1], py_labels_path, parameters=parameters, non_perm_parameters=non_perm_parameters)
+
+
 if __name__ == '__main__':
-    call_siamese_triplets()
+    call_offline()
